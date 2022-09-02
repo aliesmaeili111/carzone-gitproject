14cccf9ef6c4b4a4a90d4593b8c5e57e41283227
ali esmaeili <aliesmaeili1177@gmail.com>
2022-09-02 16:18:17 +0430

    carcone project

---
+++ cars/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import cars,car_detail,search
+
+urlpatterns = [
+    path('',cars,name='cars'),
+    path('<int:id>/',car_detail,name='car_detail'),
+    path('search/',search,name='search'),
+    ]
---
+++ contacts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import inquiry
+
+urlpatterns = [
+    path('inquiry/',inquiry,name = "inquiry"),
+
+]
